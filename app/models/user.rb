7200ae8c4e6f9358f422f84f1c7570b830f221f8
AJ Keeter <aleckeeter94@gmail.com>
2019-09-12 15:26:58 -0400

    Adds validations to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable,  and :omniauthable
   devise :database_authenticatable, :trackable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates_presence_of :first_name, :last_name
 end
