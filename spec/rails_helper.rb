d099c81e8b3919b13e0162fe088b64e36ff9d80c
AJ Keeter <aleckeeter94@gmail.com>
2019-09-18 11:53:51 -0400

    Implemented user connection with posts with tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,12 +7,16 @@ require 'rspec/rails'
 require 'spec_helper'
 require 'capybara/rails'
 
+include Warden::Test::Helpers
+Warden.test_mode!
+
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
