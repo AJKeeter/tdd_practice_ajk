30c9f22c00a6448dde1a6d3dd79e67a00e5b8df6
AJ Keeter <aleckeeter94@gmail.com>
2019-09-25 12:54:07 -0400

    Adds admin user to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
 @user = User.create(email: "test@test.com", password: "password", password_confirmation: "password", first_name: "Jon", last_name: "Schnee")
 
-puts "1 user created"
+puts "1 User created"
+
+AdminUser.create(email: "admin@test.com", password: "password", password_confirmation: "password", first_name: "Admin", last_name: "User")
+
+puts "1 Admin User created"
 
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
